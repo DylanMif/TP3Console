80f057ed1db5ac712142fa39a65d45da9c894d6e
DylanMif <dylan.miftari@etu.univ-savoie.fr>
2024-02-08 17:42:29 +0100

    Ajoutez des fichiers projet.

---
+++ TP3Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TP3Console.Models.EntityFramework;
+
+namespace TP3Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new FilmDBContext())
+            {
+                //Chargement de la catégorie Action
+                Categorie categorieAction = ctx.Categories.First(c => c.Nom == "Action");
+                Console.WriteLine("Categorie : " + categorieAction.Nom);
+                Console.WriteLine("Films : ");
+                //Chargement des films de la catégorie Action.
+                foreach (var film in categorieAction.Films) // lazy loading initiated
+                {
+                    Console.WriteLine(film.Nom);
+                }
+            }
+        }
+    }
+}
